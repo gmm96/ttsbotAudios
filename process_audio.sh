8106626eeda7d079886efd548c0eb0e05f1b271a
gmm96 <guillermo10_96@hotmail.com>
2016-12-28 15:57:32 +0100

    Process audio modified

---
+++ process_audio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mod_extension="ogg"
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <files>"
+    exit 1
+fi
+
+for audio in "$@"
+do
+	ffmpeg -i $audio -ac 1 -c:a opus -b:a 16k tmp.ogg
+	orig_name=`echo "$audio" | cut -d'.' -f1`
+	orig_extension=`echo "$audio" | cut -d'.' -f2`
+	mod_name="$orig_name.$mod_extension"
+	mv tmp.ogg $mod_name
+	if [ $orig_extension -ne "ogg"]; then
+		rm $audio
+	fi
+	git add $mod_name
+done
+
+git commit -m "Uploading $@ to repo"
+git push
